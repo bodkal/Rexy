a95b35f5b4dbcaf04eff768a6b02bbe65f191605
Your Name <you@example.com>
2022-02-24 18:38:31 +0200

    update

---
+++ lcd_meneger/src/lcd_writer.py
@@ -0,0 +1,37 @@
+import lcddriver
+from time import *
+from gpiozero import CPUTemperature
+import subprocess
+import psutil
+
+
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+
+
+# Calculate memory information
+
+res = str(subprocess.check_output(['hostname', '-I'])).split(' ')[0].replace("b'", "")
+
+while True:
+     # Get cpu statistics
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory().percent
+    
+    # Convert Bytes to MB (Bytes -> KB -> MB)
+    #available = int(memory.available/1024.0/1024.0)
+    #total = int(memory.total/1024.0/1024.0)
+
+    lcd.lcd_display_string(f"CPU: {cpu}% {round(CPUTemperature().temperature,1)}c", 1)
+    lcd.lcd_display_string(f"RAM: {memory} %" , 2)
+    sleep(5)
+    lcd.lcd_clear()
+    lcd.lcd_display_string(f"IP: ", 1)
+    lcd.lcd_display_string(f"{res}" , 2)
+    sleep(5)
+    lcd.lcd_clear()
+
+
+
+
+
--- rexy_motor_control/src/motor_control.cpp
+++ rexy_motor_control/src/motor_control.cpp
@@ -31,19 +31,11 @@ private:
   PCA9685 *pca9685 = new PCA9685();
 
 
-  int16_t min_pwm_val[12]={180,120,610,620,140,620,90,610,140,550,600,130};
-  int16_t max_pwm_val[12]={540,610,110,250,635,110,475,125,630,170,100,640};
-
-  int16_t min_angle_val[12]={25,0,0,25,0,0,25,0,0,25,0,0};
-  int16_t max_angle_val[12]={160,180,180,160,180,180,160,180,180,160,180,180};
-
-
-std::map<std::string, std::vector<int16_t> > name_id_maping = {{ "bl", {0,1,2}  },
-                                                               { "fl", {3,4,5}  },
-                                                               { "fr", {6,7,8}  },
-                                                               { "br", {9,10,11}}};
-
-
+  std::vector<int16_t> min_pwm_val;
+  std::vector<int16_t> max_pwm_val ;
+  std::vector<int16_t> min_angle_val;
+  std::vector<int16_t> max_angle_val;
+  std::map<std::string, std::vector<int16_t> > name_id_maping;
 
   float next_step(float goal, float current,float step){
 
@@ -59,42 +51,49 @@ std::map<std::string, std::vector<int16_t> > name_id_maping = {{ "bl", {0,1,2}  
 
   void read_config(){
     
-    YAML::Node config = YAML::LoadFile("/home/koby/rexy_ws/src/rexy_motor_control/config/rexy_motor_config.yaml");
+    YAML::Node config = YAML::LoadFile("/home/rexy/rexy_ws/src/rexy_motor_control/config/rexy_motor_config.yaml");
                    std::cout <<"read configuriton files ... "<<std::endl;
 
                 // loop over the positions Rectangle and print them:
-              std::map<std::string, std::vector<int16_t> > name_id_maping = config["name_id_maping"].as<std::map<std::string, std::vector<int16_t> >>();
+              name_id_maping = config["name_id_maping"].as<std::map<std::string, std::vector<int16_t> >>();
               
-              std::vector<int16_t> min_pwm_val = config["map_angel_pwm"]["min_pwm_val"].as<std::vector<int16_t>>();
-              std::vector<int16_t> max_pwm_val = config["map_angel_pwm"]["max_pwm_val"].as<std::vector<int16_t>>();
-              std::vector<int16_t> min_angle_val = config["map_angel_pwm"]["min_angle_val"].as<std::vector<int16_t>>();
-              std::vector<int16_t> max_angle_val = config["map_angel_pwm"]["max_angle_val"].as<std::vector<int16_t>>();
+              min_pwm_val = config["map_angel_pwm"]["min_pwm_val"].as<std::vector<int16_t>>();
+              max_pwm_val = config["map_angel_pwm"]["max_pwm_val"].as<std::vector<int16_t>>();
+              min_angle_val = config["map_angel_pwm"]["min_angle_val"].as<std::vector<int16_t>>();
+              max_angle_val = config["map_angel_pwm"]["max_angle_val"].as<std::vector<int16_t>>();
               
-               std::cout << name_id_maping["br"][2]<<std::endl;
-               std::cout << min_pwm_val[0]<<std::endl;
-               std::cout << max_pwm_val[0]<<std::endl;
-               std::cout << min_angle_val[0]<<std::endl;
-               std::cout << max_angle_val[0]<<std::endl;
   }
 
 
   void publish_state()
   {
-
     for(int leg = 0;leg < 4;leg++){
 
     std::vector<short> current_id =  this->name_id_maping[this->current_state.legs[leg].name];
 
-    for (int motor_index = 0;motor_index < 4;motor_index++){
-      this->current_state.legs[leg].vel[motor_index]=this->goal_state.legs[leg].vel[motor_index];
-      this->current_state.legs[leg].pos[motor_index]=
-      this-> next_step(this->goal_state.legs[leg].pos[motor_index],this->current_state.legs[leg].pos[motor_index],this->current_state.legs[leg].vel[motor_index]);
-      this->pca9685->setPWM(current_id[motor_index], 0,this->current_state.legs[leg].pos[motor_index]);
+    for (int motor_index = 0;motor_index < 3;motor_index++){
 
-      std::cout << this->current_state.legs[leg].pos[motor_index]<<"\t";
+      this->current_state.legs[leg].vel[motor_index]=this->goal_state.legs[leg].vel[motor_index];
+
+      this->current_state.legs[leg].pos[motor_index]=this-> next_step(this->goal_state.legs[leg].pos[motor_index],
+                                                                      this->current_state.legs[leg].pos[motor_index],
+                                                                      this->current_state.legs[leg].vel[motor_index]);
+
+      int pwm_to_motor=convert_agnle_to_pwm(this->current_state.legs[leg].pos[motor_index],current_id[motor_index]);
+
+      this->pca9685->setPWM(current_id[motor_index],
+                            0,
+                            pwm_to_motor);
+
+
+      std::cout << this->current_state.legs[leg].name<<" "<<motor_index<<" "<<current_id[motor_index]<<"  "<<
+      convert_agnle_to_pwm(this->current_state.legs[leg].pos[motor_index],current_id[motor_index])<<"\t";
+
+
     }
+
     }
-    std::cout << std::endl;
+        std::cout << std::endl;
 
     this->publisher->publish(this->current_state);
 
@@ -155,18 +154,22 @@ std::map<std::string, std::vector<int16_t> > name_id_maping = {{ "bl", {0,1,2}  
 public:
  
   MotorControl() : Node("motor_control")
-  {
+  { 
+    this->read_config();
+
+    
     this->goal_state=this->home();
+
     this->current_state=this->home();
 
     this->publisher = this->create_publisher<rexy_msg::msg::LegList>("cuurent_state", 10);
     this->subscription = this->create_subscription<rexy_msg::msg::LegList>("goal_state", 10, std::bind(&MotorControl::goal_state_callback, this, _1));
-    this->read_config();
     
-    /*
+    
+    
     int err = pca9685->openPCA9685();
-    printf("%d\n", err);
-    
+    //printf("%d\n", err);
+
     if (err < 0)
     {
       printf("Error: %d", pca9685->error);
@@ -178,8 +181,9 @@ public:
       pca9685->reset();
       pca9685->setPWMFrequency(60);
     }
+    std::cout<<10<<std::endl;
     this->timer_ = this->create_wall_timer(40ms, std::bind(&MotorControl::publish_state, this));
-    */
+    
   }
 };
 
